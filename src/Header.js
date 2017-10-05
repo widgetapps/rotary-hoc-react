ffefe09274cc71feea3eaa3b4fc5b5577e656082
Darryl Patterson <widgetapps@gmail.com>
2017-10-05 09:19:20 -0400

    Added social links to main nav.

--- src/Header.js
+++ src/Header.js
@@ -35,6 +35,12 @@ class Header extends Component {
                     <div class="navbar navbar-default">
                         <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse" aria-expanded="false"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
 
+                        <div class="navbar-text social-media social-media-inline pull-right">
+                            <a href="https://twitter.com/RotaryTO2018"><i class="fa fa-twitter"></i></a>
+                            <a href="https://www.facebook.com/RotaryTO2018/"><i class="fa fa-facebook"></i></a>
+                            <a href="https://www.instagram.com/RotaryTO2018/"><i class="fa fa-instagram"></i></a>
+                        </div>
+
                         <div class="navbar-collapse collapse">
                             <ul class="nav navbar-nav" id="main-menu">
                                 <li class="icon-link">
