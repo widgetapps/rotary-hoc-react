22ff8cb28c408c3fc2ff58b68f6a8e277ff10af0
Darryl Patterson <widgetapps@gmail.com>
2018-03-03 09:08:04 -0500

    Added support files.

--- src/pages/Files.js
+++ src/pages/Files.js
@@ -7,10 +7,10 @@ class Files extends Component {
     render() {
         const meta = {
             title: 'Files | Rotary International Convention Toronto 2018 (June 23-27)',
-            canonical: 'http://rotary2018.org/files',
+            canonical: 'http://rotary2018.org/support-files',
             meta: {
                 property: {
-                    'og:url': 'http://rotary2018.org/files'
+                    'og:url': 'http://rotary2018.org/support-files'
                 }
             }
         };
@@ -25,12 +25,23 @@ class Files extends Component {
                                 <div className="page-header">
                                     <h1>
                                         HOC File
-                                        <small>Here are some useful files form the HOC</small>
+                                        <small>Here are some useful files from the HOC</small>
                                     </h1>
                                 </div>
                                 <div className="row">
                                     <div className="col-md-12">
-                                        <p>Files will be posted soon.</p>
+                                        <h3>Sponsorship Documents</h3>
+                                        <ul>
+                                            <li><a href="/support-files/HOC_IC18_Sponsorship_Brochure.pdf">Sponsorship Brochure (PDF)</a></li>
+                                            <li><a href="/support-files/2018_Toronto_Rotary_Convention-Sponsorship_Presentation.pptx">Sponsorship Powerpoint Presentation (PPTX)</a></li>
+                                            <li><a href="/support-files/Sponsorship_Supporter_Levels.pdf">Sponsorship Supporter Levels (PDF)</a></li>
+                                            <li><a href="/support-files/sponsorship_cover_letter.docx">Sponsorship Cover Letter (DOCX(</a></li>
+                                        </ul>
+
+                                        <h3>Event Documents</h3>
+                                        <ul>
+                                            <li><a href="/support-files/RI18_HOC_Events_Brochure.pdf">Events Brochure</a></li>
+                                        </ul>
                                     </div>
                                 </div>
                             </div>
